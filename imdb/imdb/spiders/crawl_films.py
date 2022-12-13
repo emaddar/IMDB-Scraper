1c85ee43331d43040284958c96f3b175cb19982c
Emad_Simplon <e.a.darwich@gmail.com>
2022-12-13 16:17:24 +0100

    all films

--- imdb/imdb/spiders/crawl_films.py
+++ imdb/imdb/spiders/crawl_films.py
@@ -16,14 +16,26 @@ class CrawlFilmsSpider(CrawlSpider):
 
 
     le_films_details = LinkExtractor(restrict_xpaths="//td[@class='titleColumn']/a")
+    le_films_details_acteurs = LinkExtractor(restrict_xpaths='//*[@id="iconContext-chevron-right-inline"]/path')
+    
+    
     rule_films_details = Rule(le_films_details,
                              callback='parse_item',
                               follow=False)
+
+    
+    
+    rule_films_details_acts = Rule(le_films_details_acteurs,
+                            callback='parse_item',
+                            follow=True)
+                            
     rules = (
-      rule_films_details  ,
+      rule_films_details  , rule_films_details_acts
     )
 
     def parse_item(self, response):
+
+        # languages = response.xpath('//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[13]/div[2]/ul/li[4]/div/ul/li/a/text()').extract()
         title = response.xpath('//h1/text()').extract()
         year = response.xpath('//span[@class="sc-8c396aa2-2 itZqyK"]/text()').extract_first()
         duration = response.xpath('//li[@class="ipc-inline-list__item"]/text()').extract()
@@ -34,14 +46,17 @@ class CrawlFilmsSpider(CrawlSpider):
         Director = response.xpath('//a[@class="ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link"]/text()').extract_first()
         Acteurs = response.xpath('//a[@class="sc-bfec09a1-1 gfeYgX"]/text()').extract()
         Public = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/div/ul/li[2]/a/text()').extract()
-        Pays = response.xpath('a[@class="ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link"]/text()').extract()
-        color = response.xpath('//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[13]/div[2]/ul/li[4]/div/ul/li/a/text()').extract()
-        
-        # original_lang = 
+        Pays = response.xpath("/html/body/div[2]/main/div/section[1]/div/section/div/div[1]/section[@class='ipc-page-section ipc-page-section--base celwidget']/div[@class='sc-f65f65be-0 ktSkVi']/ul[@class='ipc-metadata-list ipc-metadata-list--dividers-all ipc-metadata-list--base']/li[@class='ipc-metadata-list__item'][1]/div[@class='ipc-metadata-list-item__content-container']/ul[@class='ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--inline ipc-metadata-list-item__list-content base']/li[@class='ipc-inline-list__item']/a[@class='ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link']/text()").extract()
+        languages = response.xpath('//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section/div[2]/ul/li[4]/div/ul/li/a/text()').extract()
+        avis = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[2]/ul/li[1]/a/span/span[1]/text()').extract()
+        Metacritic = response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[2]/ul/li[2]/a/span/span[1]/text()').extract()
         
 
         yield{ 
-            # 'Pays' : Pays,
+            'Metacritic' : Metacritic,
+            'avis' : avis,
+            'Pays' : Pays,
+            'languages' : languages,
             'title': title,
             'year' : year,
             'duration' : duration,
@@ -51,10 +66,13 @@ class CrawlFilmsSpider(CrawlSpider):
             'Director' : Director,
             'Acteurs' : Acteurs,
             'Public' : Public,
-            'color' : color,
 
 
 
             'Descriptions' : Descriptions,
             
         }
+
+
+
+
